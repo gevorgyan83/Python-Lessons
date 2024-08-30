e022ad16b8f11d8b1705b4c1ecefd3787bada575
gevorgyan83 <ishkhangevorgyan53@gmail.com>
2024-08-30 16:25:06 +0400

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,7 @@
+previous_num = 0
+for current_num in range(10):
+    num_sum = current_num + previous_num
+
+    print(f" Current Number: {current_num}, Previous Number: {previous_num}, Sum: {num_sum}")
+
+    previous_num = current_num
